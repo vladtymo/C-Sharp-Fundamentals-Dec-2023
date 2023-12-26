16b1d234f4e4be42fdb7c0fbb4a57645e1077100
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-12-26 19:35:30 +0200

    Arrays

---
+++ 03_arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace _03_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- Arrays --------------");
+
+            string[] colors = ["Red", "Green", "blue", "Purple"];
+
+            Console.WriteLine($"Colors: {colors.Length}"); // 4
+
+            // [0] - first element
+            colors[2] = "Black";
+
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.WriteLine(colors[i] + " ");
+            }
+            Console.WriteLine();
+
+            // TASK: show colors abbreviation (RGB...)
+            foreach (string item in colors)
+            {
+                // item = "New"; - can not change the value
+                Console.Write(char.ToUpper(item[0]));
+            }
+            Console.WriteLine();
+
+            // TODO: investigate StringBuulder
+        }
+    }
+}
