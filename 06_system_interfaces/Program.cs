27f0047f56d9426cb3af2681c59a3f59cd320093
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-01-02 20:17:19 +0200

    Interfaces example.

---
+++ 06_system_interfaces/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+
+namespace _06_system_interfaces
+{
+    class Player : IComparable<Player>, ICloneable
+    {
+        public int Number { get; set; }
+        public string? Name { get; set; }
+        public int Goals { get; set; }
+        public int Games { get; set; }
+        public double Productivity => Math.Round((double)Goals / Games, 1);
+
+        public object Clone()
+        {
+            // shallow copy - copy value types only
+            Player copy = (Player)this.MemberwiseClone();
+
+            // deep copy - copy all referece objects
+            copy.Name = (string)this.Name?.Clone();
+            // copy other reference types...
+
+            return copy;
+        }
+
+        public int CompareTo(Player? other)
+        {
+            // compare [this] and [other]
+            // result: 0 - equals, <0 this < other, >0 this > other
+            return this.Productivity.CompareTo(other.Productivity) * -1;
+        }
+
+
+
+        //public int CompareTo(Player? other)
+        //{
+        //    // you can write custom logic here...
+        //    return this.Productivity.CompareTo(other.Productivity);
+        //}
+
+        public override string ToString()
+        {
+            return $"Player [{Number}]: {Goals}/{Games} - KPD: {Productivity}";
+        }
+    }
+
+    class Team : IEnumerable
+    {
+        private Player[] players;
+        public string Name { get; set; }
+
+        public Team(string name)
+        {
+            Name = name;
+
+            var rand = new Random();
+
+            int count = rand.Next(5, 10);
+            players = new Player[count];
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i] = new Player()
+                {
+                    Number = rand.Next(100),
+                    Games = rand.Next(20, 200),
+                    Goals = rand.Next(10, 500)
+                };
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return players.GetEnumerator();
+        }
+        public void Sort()
+        {
+            // Array.Sort() - requires IComparable<> interface from array item
+            Array.Sort(this.players);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Team team = new Team("Dream Team");
+
+            team.Sort();
+
+            // foreach() - requires IEnumerable interface implementation
+            foreach (var p in team)
+            {
+                Console.WriteLine(p);
+            }
+
+            Player player = new Player() { Number = 9, Games = 44, Goals = 67 };
+
+            Player copy = (Player)player.Clone();
+
+            Console.WriteLine("Player copy...");
+            player.Games++;
+            Console.WriteLine(copy);
+        }
+    }
+}
---
+++ 07_interface_example/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace _07_interface_example
+{
+    public interface IMovable
+    {
+        int X { get; set; }
+        int Y { get; set; }
+
+        // default realization
+        void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+    public interface IPrintable
+    {
+        char Filler { get; }
+        ConsoleColor Color { get; }
+        void Print();
+    }
+
+    public interface IShape : IPrintable, IMovable
+    {
+        string Name { get; }
+    }
+
+    public class Rectangle : IShape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public char Filler { get; init; }
+        public ConsoleColor Color { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Name { get; } = "Rectangle";
+
+        public void Print()
+        {
+            Console.ForegroundColor = Color;
+            for (int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(X, Y + i);
+                for (int k = 0; k < Width; k++)
+                {
+                    Console.Write(Filler);
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public int Height { get; set; }
+        public char Filler { get; set; }
+        public string Name { get; } = "Triangle";
+        public ConsoleColor Color { get; set; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Print()
+        {
+            Console.ForegroundColor = Color;
+            for (int i = 0; i < Height; i++)
+            {
+                Console.SetCursorPosition(X, Y + i);
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(Filler);
+                }
+                Console.WriteLine();
+            }
+            Console.ResetColor();
+        }
+    }
+
+    internal class Program
+    {
+        static void TestShape(IShape shape)
+        {
+            Console.WriteLine($"{shape.Name} filler: {shape.Filler}");
+
+            //shape.Move(10, 10);
+            shape.Print();
+        }
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new()
+            {
+                Height = 6,
+                Width = 20,
+                X = 5,
+                Y = 5,
+                Color = ConsoleColor.DarkRed,
+                Filler = '#' // init here
+            };
+            Triangle triangle = new()
+            {
+                Height = 12,
+                X = 30,
+                Y = 10,
+                Color = ConsoleColor.Cyan,
+                Filler = '*'
+            };
+
+            //rectangle.Filler = 'X'; // error with init-only property
+            //rectangle.Print();
+            //triangle.Print();
+
+            TestShape(rectangle);
+            TestShape(triangle);
+        }
+    }
+}
